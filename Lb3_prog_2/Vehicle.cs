d325fda8edd710d3b13656ddd49c30979cc555da
elecshen <51473599+elecshen@users.noreply.github.com>
2022-12-06 17:07:34 +0700

    Класс Vehicle

---
+++ Lb3_prog_2/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lb3_prog_2
+{
+    internal abstract class Vehicle : INotifyPropertyChanged
+    {
+        protected static double distRatio = 0;
+        protected static double startCost = 0;
+        public double CurrentLoad { get; }
+        public double LoadCapacity { get; }
+        private ObservableCollection<Product> cargo;
+        public ReadOnlyObservableCollection<Product> Cargo;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public static double CalculateCost(double distance, double weight)
+        {
+            return distRatio * distance * weight + startCost;
+        }
+
+        public void AddCargo(Product product)
+        {
+            cargo.Add(product);
+        }
+
+        public void RemuveCargo(int id)
+        {
+            cargo.RemoveAt(id);
+        }
+
+        public abstract void/*waybill*/ SendByRoute(/*cargo*/);
+    }
+}
