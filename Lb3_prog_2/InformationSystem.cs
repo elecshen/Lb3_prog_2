db99a44aa5ad5b17637c72497247773d6595aa93
elecshen <51473599+elecshen@users.noreply.github.com>
2022-12-06 15:46:13 +0700

    Добавлен главный класс модели

---
+++ Lb3_prog_2/InformationSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lb3_prog_2
+{
+    internal class InformationSystem
+    {
+        ObservableCollection<Product> products;
+    }
+}
